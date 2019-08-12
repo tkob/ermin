ff4d6a47689e791459de24a762a3bedf33d40b05
tkob <ether4@gmail.com>
2019-08-12 12:50:59 +0900

    Remove namespace from ErminName

--- src/main/java/yokohama/lang/ermin/attribute/ErminName.java
+++ src/main/java/yokohama/lang/ermin/attribute/ErminName.java
@@ -2,29 +2,21 @@ package yokohama.lang.ermin.attribute;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import lombok.Value;
 
 @Value
 public class ErminName {
-    private Optional<List<String>> nameSpace;
 
-    private List<String> localNameParts;
+    private List<String> parts;
 
     public String toString() {
-        return nameSpace.map(ns -> ns.stream().collect(Collectors.joining("_")) + "::")
-                .orElse("") + localNameParts.stream().collect(Collectors.joining("_"));
-    }
-
-    public static ErminName fromSnakeNS(String nameSpace, String localName) {
-        return new ErminName(Optional.of(Arrays.asList(nameSpace.split("_+"))), Arrays
-                .asList(localName.split("_+")));
+        return parts.stream().collect(Collectors.joining("_"));
     }
 
     public static ErminName fromSnake(String localName) {
-        return new ErminName(Optional.empty(), Arrays.asList(localName.split("_+")));
+        return new ErminName(Arrays.asList(localName.split("_+")));
     }
 
 }
