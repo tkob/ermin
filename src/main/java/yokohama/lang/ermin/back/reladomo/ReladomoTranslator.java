757ef19c49002ec4fbd05c01c633f477c78a21bf
tkob <ether4@gmail.com>
2019-08-17 10:28:06 +0900

    Remove relationships for codes
    as they are almost useless for Reladomo-generated classes:
    code values can be accessed with normal methods for attributes.

--- src/main/java/yokohama/lang/ermin/back/reladomo/ReladomoTranslator.java
+++ src/main/java/yokohama/lang/ermin/back/reladomo/ReladomoTranslator.java
@@ -12,20 +12,9 @@ import yokohama.lang.ermin.front.CodeResolver;
 import yokohama.lang.ermin.front.ErminTuple;
 import yokohama.lang.ermin.front.FrontEndProcessor;
 import yokohama.lang.ermin.front.Resolver;
-import yokohama.lang.ermin.type.ErminBlobType;
-import yokohama.lang.ermin.type.ErminCharType;
-import yokohama.lang.ermin.type.ErminClobType;
-import yokohama.lang.ermin.type.ErminDateType;
-import yokohama.lang.ermin.type.ErminDecimalType;
-import yokohama.lang.ermin.type.ErminIntegerType;
-import yokohama.lang.ermin.type.ErminStringCodeType;
-import yokohama.lang.ermin.type.ErminTypeVisitor;
-import yokohama.lang.ermin.type.ErminVarCharType;
 import yokohama.lang.reladomo.AttributeType;
-import yokohama.lang.reladomo.CardinalityType;
 import yokohama.lang.reladomo.MithraObjectType;
 import yokohama.lang.reladomo.ObjectFactory;
-import yokohama.lang.reladomo.RelationshipType;
 
 public class ReladomoTranslator {
 
@@ -77,69 +66,12 @@ public class ReladomoTranslator {
         mithraObject.setClassName(entity.getName().toUpperCamel());
         mithraObject.setDefaultTable(entity.getName().toSnake());
         final List<AttributeType> attributes = mithraObject.getAttribute();
-        final List<RelationshipType> relationships = mithraObject.getRelationship();
 
         accumulatePrimaryKeys(entity, entityResolver, codeResolver, attributes);
 
         // add non-key attributes
         attributes.addAll(entity.getAttributes().stream().map(attribute -> toAttribute(
                 attribute, codeResolver)).collect(Collectors.toList()));
-
-        // add code relationships
-        entity.getAttributes().forEach(attribute -> {
-            attribute.getType().accept(new ErminTypeVisitor<Void>() {
-
-                @Override
-                public Void visitCharType(ErminCharType charType) {
-                    return null;
-                }
-
-                @Override
-                public Void visitVarCharType(ErminVarCharType varCharType) {
-                    return null;
-                }
-
-                @Override
-                public Void visitClobType(ErminClobType clobType) {
-                    return null;
-                }
-
-                @Override
-                public Void visitBlobType(ErminBlobType blobType) {
-                    return null;
-                }
-
-                @Override
-                public Void visitDecimalType(ErminDecimalType decimalType) {
-                    return null;
-                }
-
-                @Override
-                public Void visitIntegerType(ErminIntegerType integerType) {
-                    return null;
-                }
-
-                @Override
-                public Void visitDateType(ErminDateType dateType) {
-                    return null;
-                }
-
-                @Override
-                public Void visitStringCodeType(ErminStringCodeType stringCodeType) {
-                    final RelationshipType relationshipType = factory
-                            .createRelationshipType();
-                    relationshipType.setName(attribute.getName().toLowerCamel());
-                    relationshipType.setRelatedObject(stringCodeType.getName().toUpperCamel());
-                    relationshipType.setCardinality(CardinalityType.MANY_TO_ONE);
-                    relationshipType.setValue("this." + attribute.getName().toLowerCamel()
-                            + " = " + stringCodeType.getName().toUpperCamel() + "."
-                            + stringCodeType.getName().toLowerCamel());
-                    relationships.add(relationshipType);
-                    return null;
-                }
-            });
-
-        });
 
         return mithraObject;
     }
