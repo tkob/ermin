35f4ec25e1cfe429094964677e728c5032c05519
tkob <ether4@gmail.com>
2019-08-17 22:52:20 +0900

    Add applyBiFunction method

--- src/main/java/yokohama/lang/ermin/relationship/ErminRelationship.java
+++ src/main/java/yokohama/lang/ermin/relationship/ErminRelationship.java
@@ -1,5 +1,8 @@
 package yokohama.lang.ermin.relationship;
 
+import java.util.Optional;
+import java.util.function.BiFunction;
+
 import lombok.Value;
 import yokohama.lang.ermin.attribute.ErminName;
 
@@ -11,5 +14,62 @@ public class ErminRelationship {
 
     public int getArity() {
         return exp.getArity();
+    }
+
+    public <R> Optional<R> applyBiFunction(
+            BiFunction<ErminAtomicRelationshipExp, ErminAtomicRelationshipExp, R> f) {
+
+        return this.getExp().accept(new ErminRelationshipExpVisitor<Optional<R>>() {
+
+            @Override
+            public Optional<R> visitAtomicRelationshipExp(
+                    ErminAtomicRelationshipExp atomicRelationshipExp) {
+
+                // unary relationship
+                return Optional.empty();
+            }
+
+            @Override
+            public Optional<R> visitProductRelationshipExp(
+                    ErminProductRelationshipExp productRelationshipExp) {
+
+                return productRelationshipExp.getLeft().accept(
+                        new ErminRelationshipExpVisitor<Optional<R>>() {
+
+                            @Override
+                            public Optional<R> visitAtomicRelationshipExp(
+                                    ErminAtomicRelationshipExp left) {
+
+                                return productRelationshipExp.getRight().accept(
+                                        new ErminRelationshipExpVisitor<Optional<R>>() {
+
+                                            @Override
+                                            public Optional<R> visitAtomicRelationshipExp(
+                                                    ErminAtomicRelationshipExp right) {
+
+                                                // binary relationship
+                                                return Optional.ofNullable(f.apply(left, right));
+                                            }
+
+                                            @Override
+                                            public Optional<R> visitProductRelationshipExp(
+                                                    ErminProductRelationshipExp productRelationshipExp) {
+
+                                                // multi relationship
+                                                return Optional.empty();
+                                            }
+                                        });
+                            }
+
+                            @Override
+                            public Optional<R> visitProductRelationshipExp(
+                                    ErminProductRelationshipExp productRelationshipExp) {
+
+                                // multi relationship
+                                return Optional.empty();
+                            }
+                        });
+            }
+        });
     }
 }
